0c11d1ea398e6c6dbeff6a7cb284f0a1fdb88312
NabinKhatiwada <nabinkumarkhatiwada@gmail.com>
2021-10-09 06:34:05 +0545

    Spring booot profiles

---
+++ src/main/java/com/school/ManagementApplication.java
@@ -0,0 +1,15 @@
+package com.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementApplication.class, args);
+	}
+
+
+
+}
